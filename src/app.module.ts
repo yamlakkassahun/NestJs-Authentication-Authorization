b88b750c3f6f745fb78d9fa7e248130ba576bc57
yamlakkassahun <yamlak.k@gmail.com>
2022-05-30 17:04:34 +0300

    a commplet nest js starter project with jwt authentication and role based authorization

---
+++ src/app.module.ts
@@ -0,0 +1,38 @@
+import { Module } from '@nestjs/common';
+
+//packages
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { APP_FILTER } from '@nestjs/core';
+import { MongooseModule } from '@nestjs/mongoose';
+
+//external imports
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { AllExceptionsFilter } from './core/all-exceptions.filter';
+import { FeedsModule } from './feeds/feeds.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      useFactory: (configService: ConfigService) => ({
+        uri: 'mongodb://127.0.0.1:27017/NestJs',
+      }),
+      inject: [ConfigService],
+    }),
+    AuthModule,
+    FeedsModule,
+  ],
+  controllers: [AppController],
+  providers: [
+    AppService,
+    {
+      provide: APP_FILTER,
+      useClass: AllExceptionsFilter,
+    },
+  ],
+})
+export class AppModule {}
